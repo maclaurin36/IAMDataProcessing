8b532cb028566dc47e170b70c76785d3645889fb
Jesse <jesseclarkwins@gmail.com>
2023-03-25 19:15:18 -0600

    add dir

---
+++ imgtxtenh/src/CMakeFiles/imgtxtenh.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jesse/Laia/egs/iam/imgtxtenh/src/htrprep.c" "/home/jesse/Laia/egs/iam/imgtxtenh/src/CMakeFiles/imgtxtenh.dir/htrprep.c.o"
+  "/home/jesse/Laia/egs/iam/imgtxtenh/src/imgconv.c" "/home/jesse/Laia/egs/iam/imgtxtenh/src/CMakeFiles/imgtxtenh.dir/imgconv.c.o"
+  "/home/jesse/Laia/egs/iam/imgtxtenh/src/imgio_magick.c" "/home/jesse/Laia/egs/iam/imgtxtenh/src/CMakeFiles/imgtxtenh.dir/imgio_magick.c.o"
+  "/home/jesse/Laia/egs/iam/imgtxtenh/src/imgtxtenh.c" "/home/jesse/Laia/egs/iam/imgtxtenh/src/CMakeFiles/imgtxtenh.dir/imgtxtenh.c.o"
+  "/home/jesse/Laia/egs/iam/imgtxtenh/src/intimg.c" "/home/jesse/Laia/egs/iam/imgtxtenh/src/CMakeFiles/imgtxtenh.dir/intimg.c.o"
+  "/home/jesse/Laia/egs/iam/imgtxtenh/src/log.c" "/home/jesse/Laia/egs/iam/imgtxtenh/src/CMakeFiles/imgtxtenh.dir/log.c.o"
+  "/home/jesse/Laia/egs/iam/imgtxtenh/src/mem.c" "/home/jesse/Laia/egs/iam/imgtxtenh/src/CMakeFiles/imgtxtenh.dir/mem.c.o"
+  "/home/jesse/Laia/egs/iam/imgtxtenh/src/objs.c" "/home/jesse/Laia/egs/iam/imgtxtenh/src/CMakeFiles/imgtxtenh.dir/objs.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ imgtxtenh/src/CMakeFiles/imgtxtenh.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgtxtenh.dir/htrprep.c.o"
+  "CMakeFiles/imgtxtenh.dir/imgconv.c.o"
+  "CMakeFiles/imgtxtenh.dir/imgio_magick.c.o"
+  "CMakeFiles/imgtxtenh.dir/imgtxtenh.c.o"
+  "CMakeFiles/imgtxtenh.dir/intimg.c.o"
+  "CMakeFiles/imgtxtenh.dir/log.c.o"
+  "CMakeFiles/imgtxtenh.dir/mem.c.o"
+  "CMakeFiles/imgtxtenh.dir/objs.c.o"
+  "imgtxtenh"
+  "imgtxtenh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/imgtxtenh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
